a30d043feac227b60071d370dd3e695dd029fdd8
Manson Xasthur <mansonx13@gmail.com>
2017-12-24 12:31:59 +0200

    edit page title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
+    <meta name="description" content="GMS Group app control panel">
     <meta name="author" content="GeeksLabs">
-    <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
+    <meta name="keyword" content="GMS, Company, App, Control Panel">
     <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
 
-    <title>GMS Admin Template</title>
+    <title>GMS Admin Area</title>
 
     <!-- Bootstrap CSS -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
